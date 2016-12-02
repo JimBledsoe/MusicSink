2dc36fd01082386b7b100a47cac947a03e6f4440
JimBledsoe <jim@acceleration.net>
2016-12-02 06:12:43 -0500

    Added removable media combobox

--- MusicSink/MainWindow.xaml.cs
+++ MusicSink/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,12 +25,20 @@ namespace MusicSink
         public MainWindow()
         {
             InitializeComponent();
+
+            // Set sane defaults
+            remoteMasterPath.Text = null;
+            localMasterPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            var driveList = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Removable);
+
+            // Set last used settings
         }
 
         private void remoteMasterBrowseButton_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new System.Windows.Forms.FolderBrowserDialog();
             dialog.Description = "Select the folder on the network where the remote master music files are stored.";
+            dialog.SelectedPath = remoteMasterPath.Text;
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 remoteMasterPath.Text = dialog.SelectedPath;
@@ -40,11 +49,16 @@ namespace MusicSink
         {
             var dialog = new System.Windows.Forms.FolderBrowserDialog();
             dialog.Description = "Select the folder on this computer where the local master music files are stored.";
-            dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            dialog.SelectedPath = localMasterPath.Text;
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 localMasterPath.Text = dialog.SelectedPath;
             }
+        }
+
+        private void removableDrives_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
 
         private void scanMasterRemoteButton_Click(object sender, RoutedEventArgs e)
