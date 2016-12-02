7f2be41c7d4b04096f4989a14257c914bbd039e0
JimBledsoe <jim@acceleration.net>
2016-12-02 00:01:54 -0500

    Initial dialog checkin

---
+++ MusicSink/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace MusicSink
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void remoteMasterBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Select the folder on the network where the remote master music files are stored.";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                remoteMasterPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void localMasterBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Select the folder on this computer where the local master music files are stored.";
+            dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                localMasterPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void scanMasterRemoteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
